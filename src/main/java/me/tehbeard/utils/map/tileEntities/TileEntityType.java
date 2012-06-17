b8dd7600031b90d214a7a8c6cf1372d4a13a00a0
James <beardyjames@live.co.uk>
2012-06-17 16:11:03 +0100

    Properly Merged

--- src/main/java/me/tehbeard/utils/map/tileEntities/TileEntityType.java
+++
@@ -1,9 +0,0 @@
-package me.tehbeard.utils.map.tileEntities;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface TileEntityType {
-String id();
-}
