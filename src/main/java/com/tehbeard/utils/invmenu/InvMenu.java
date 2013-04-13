ec02a4ea2d6313435b31c43d9baa1c2b39570d76
James <beardyjames@live.co.uk>
2013-04-13 08:24:22 +0100

    Add GUI menu control made out of a chest inventory

---
+++ src/main/java/com/tehbeard/utils/invmenu/InvMenu.java
@@ -0,0 +1,48 @@
+package com.tehbeard.utils.invmenu;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.plugin.Plugin;
+
+/**
+ * Class to add gui menus to minecraft using inventories
+ * @author James
+ *
+ */
+public abstract class InvMenu implements InventoryHolder, Listener{
+
+    private Inventory inv;
+    
+    public InvMenu(Plugin plugin,int rows,String title){
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        inv = Bukkit.createInventory(this, Math.max(0,Math.min(rows,6))*9, title);
+    }
+    
+    
+    public Inventory getInventory() {
+        return inv;
+    }
+
+    @SuppressWarnings("deprecation")
+    @EventHandler(priority=EventPriority.HIGHEST)
+    public void onClick(InventoryClickEvent event){
+        if(event.getInventory().getHolder() != this){
+            return;
+        }
+        event.setCancelled(true);
+        onEvent(event);
+        
+        for(HumanEntity he : event.getViewers()){
+            ((Player)he).updateInventory();
+        }
+    }
+    
+    protected abstract void onEvent(InventoryClickEvent event);
+}
