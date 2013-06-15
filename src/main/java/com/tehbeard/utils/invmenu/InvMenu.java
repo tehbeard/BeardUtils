4146dd2ef39b310e75f090cd5c36b79fadff0066
James <beardyjames@live.co.uk>
2013-06-15 22:45:49 +0100

    Add support for inventory menu painting

--- src/main/java/com/tehbeard/utils/invmenu/InvMenu.java
+++ src/main/java/com/tehbeard/utils/invmenu/InvMenu.java
@@ -5,8 +5,10 @@ import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.plugin.Plugin;
@@ -23,6 +25,10 @@ public abstract class InvMenu implements InventoryHolder, Listener{
     public InvMenu(Plugin plugin,int rows,String title){
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
         inv = Bukkit.createInventory(this, Math.max(0,Math.min(rows,6))*9, title);
+    }
+    
+    public void cleanup(){
+        HandlerList.unregisterAll(this);
     }
     
     
@@ -44,5 +50,21 @@ public abstract class InvMenu implements InventoryHolder, Listener{
         }
     }
     
+    @EventHandler(priority=EventPriority.HIGHEST)
+    public void onPaintEvent(InventoryDragEvent event){
+        if(event.getInventory().getHolder() != this){
+            return;
+        }
+        event.setCancelled(true);
+        onPaint(event);
+        
+        for(HumanEntity he : event.getViewers()){
+            ((Player)he).updateInventory();
+        }
+    }
+    
+    protected abstract void onPaint(InventoryDragEvent event);
+
+
     protected abstract void onEvent(InventoryClickEvent event);
 }
