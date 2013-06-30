60e67cc2064113f18c227e927eb63d5fe6e858a8
James <beardyjames@live.co.uk>
2013-06-30 17:24:10 +0100

    Work on class discovery

---
+++ src/main/java/me/tehbeard/utils/addons/ClassDiscoverer.java
@@ -0,0 +1,16 @@
+package me.tehbeard.utils.addons;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Locates classes in a collection of classpaths based on 
+ * @author James
+ *
+ */
+public class ClassDiscoverer {
+    
+    List<URL> classpaths = new ArrayList<URL>();
+
+}
