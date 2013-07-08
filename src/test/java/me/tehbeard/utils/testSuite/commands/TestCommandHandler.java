75b9393db37620a6739fb1e4db0192c3755fe561
James <beardyjames@live.co.uk>
2013-07-08 02:53:59 +0100

    disable tests, re-enable later when we have mock

--- src/test/java/me/tehbeard/utils/testSuite/commands/TestCommandHandler.java
+++ src/test/java/me/tehbeard/utils/testSuite/commands/TestCommandHandler.java
@@ -1,50 +1,51 @@
-package me.tehbeard.utils.testSuite.commands;
-
-
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import me.tehbeard.utils.commands.CommandHandler;
-import me.tehbeard.utils.testSuite.fake.FakePlayer;
-
-public class TestCommandHandler {
-/*
-    CommandHandler handler;
-    
-    
-    @Before
-    public void setup(){
-        handler = new CommandHandler(null);
-        
-    }
-    
-    
-    @Test
-    public void testAddCommand(){
-        handler.addCommand(ACommand.class);
-        
-    }
-    
-    @Test
-    public void testGetInfo(){
-        handler.addCommand(ACommand.class);
-        assertNotNull(handler.getInfo("command"));
-    }
-    
-    
-    
-    @Test
-    public void testArgPackPassing(){
-        handler.addCommand(BCommand.class);
-        //CommandInfo ci = handler.getInfo("create");
-        
-        
-        handler.executeCommand(new FakePlayer("bob"), "create steveAB -type creeper -a -c \"foo bar\"");
-        
-        
-        //
-    }
-   */
-}
+//package me.tehbeard.utils.testSuite.commands;
+//
+//
+//import static org.junit.Assert.*;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import me.tehbeard.utils.commands.CommandHandler;
+//import me.tehbeard.utils.testSuite.fake.FakePlayer;
+//
+//@SuppressWarnings("unused")
+//public class TestCommandHandler {
+///*
+//    CommandHandler handler;
+//    
+//    
+//    @Before
+//    public void setup(){
+//        handler = new CommandHandler(null);
+//        
+//    }
+//    
+//    
+//    @Test
+//    public void testAddCommand(){
+//        handler.addCommand(ACommand.class);
+//        
+//    }
+//    
+//    @Test
+//    public void testGetInfo(){
+//        handler.addCommand(ACommand.class);
+//        assertNotNull(handler.getInfo("command"));
+//    }
+//    
+//    
+//    
+//    @Test
+//    public void testArgPackPassing(){
+//        handler.addCommand(BCommand.class);
+//        //CommandInfo ci = handler.getInfo("create");
+//        
+//        
+//        handler.executeCommand(new FakePlayer("bob"), "create steveAB -type creeper -a -c \"foo bar\"");
+//        
+//        
+//        //
+//    }
+//   */
+//}
