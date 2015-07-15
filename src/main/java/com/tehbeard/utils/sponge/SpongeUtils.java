3f78857ba9edd772cdc33e3ec3aae2d52b8de39f
James <beardyjames@live.co.uk>
2015-07-15 22:17:53 +0100

    All glory to hypno-sponge. (re-enable SpongeUtils)

--- src/main/java/com/tehbeard/utils/sponge/SpongeUtils.java
+++ src/main/java/com/tehbeard/utils/sponge/SpongeUtils.java
@@ -1,25 +1,24 @@
-//package com.tehbeard.utils.sponge;
-//
-//import com.tehbeard.utils.Vec3;
-//import org.spongepowered.api.math.Vector3d;
-//import org.spongepowered.api.math.Vectors;
-//import org.spongepowered.api.world.Location;
-//
-///**
-// *
-// * @author James
-// */
-//public class SpongeUtils {
-//    
-//    public static Location vec3ToLocation(Vec3 vec){
-//        return new Location(null, vec3ToVector(vec));
-//    }
-//    
-//    public static Vector3d vec3ToVector(Vec3 vec){
-//        return Vectors.create3d(vec.x, vec.y, vec.z);
-//    }
-//    
-//    public static Vec3 vectorToVec3(Vector3d vec){
-//        return new Vec3(vec.getX(), vec.getY(), vec.getZ());
-//    }
-//}
+package com.tehbeard.utils.sponge;
+
+import com.flowpowered.math.vector.Vector3d;
+import com.tehbeard.utils.Vec3;
+import org.spongepowered.api.world.Location;
+
+/**
+ *
+ * @author James
+ */
+public class SpongeUtils {
+    
+    public static Location vec3ToLocation(Vec3 vec){
+        return new Location(null, vec3ToVector(vec));
+    }
+    
+    public static Vector3d vec3ToVector(Vec3 vec){
+        return new Vector3d(vec.x, vec.y, vec.z);
+    }
+    
+    public static Vec3 vectorToVec3(Vector3d vec){
+        return new Vec3(vec.getX(), vec.getY(), vec.getZ());
+    }
+}
